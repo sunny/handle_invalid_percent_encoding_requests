f099fbfcf75c9eb63d744f985028caf174bc5b8e
Sunny Ripert <sunny@sunfox.org>
2014-07-22 13:58:39 +0200

    Fix adding middleware

--- lib/handle_invalid_percent_encoding_requests/engine.rb
+++ lib/handle_invalid_percent_encoding_requests/engine.rb
@@ -8,8 +8,8 @@ module HandleInvalidPercentEncodingRequests
       # NOTE: These must be in this order relative to each other.
       # The middleware just raises for encoding errors it doesn't cover,
       # so it must run after (= be inserted before) Rack::UTF8Sanitizer.
-      config.middleware.insert 0, Middleware
-      config.middleware.insert 0, Rack::UTF8Sanitizer
+      app.middleware.insert 0, Middleware
+      app.middleware.insert 0, Rack::UTF8Sanitizer
     end
   end
 
