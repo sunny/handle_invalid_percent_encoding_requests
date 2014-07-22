8f43f5958185081d4d0a9291b021fe16ecf6d4a9
Sunny Ripert <sunny@sunfox.org>
2014-07-22 14:09:54 +0200

    Extract method

--- lib/handle_invalid_percent_encoding_requests/middleware.rb
+++ lib/handle_invalid_percent_encoding_requests/middleware.rb
@@ -1,39 +1,38 @@
+# Via https://gist.github.com/bf4/d26259acfa29f3b9882b#file-exception_app-rb
+
 module HandleInvalidPercentEncodingRequests
 
-
-  # Via https://gist.github.com/bf4/d26259acfa29f3b9882b#file-exception_app-rb
   class Middleware
-    DEFAULT_CONTENT_TYPE = 'text/html'
-    DEFAULT_CHARSET      = 'utf-8'
-
-    attr_reader :logger
     def initialize(app, stdout=STDOUT)
       @app = app
       @logger = defined?(Rails.logger) ? Rails.logger : Logger.new(stdout)
     end
 
     def call(env)
-      begin
-        # calling env.dup here prevents bad things from happening
-        request = Rack::Request.new(env.dup)
+      # calling env.dup here prevents bad things from happening
+      request = Rack::Request.new(env.dup)
 
-        # calling request.params is sufficient to trigger the error
-        # see https://github.com/rack/rack/issues/337#issuecomment-46453404
-        request.params
-        @app.call(env)
+      # calling request.params is sufficient to trigger the error see
+      # https://github.com/rack/rack/issues/337#issuecomment-46453404
+      request.params
+      @app.call(env)
 
-      rescue ArgumentError => e
-        raise unless e.message =~ /invalid %-encoding/
+    # Rescue from that specific ArgumentError
+    rescue ArgumentError => e
+      raise unless e.message =~ /invalid %-encoding/
+      error_response
+    end
 
-        logger.info "Bad request. Returning 400 due to #{e.message}" + \
-                    " from request with env #{request.inspect}"
 
-        body = "Bad Request"
-        return [400, {
-                        'Content-Type' => "text/plain; charset=utf-8",
-                        'Content-Length' => body.bytesize.to_s
-                      }, [body]]
-      end
+    private
+
+    def error_response
+      @logger.info "Bad request. Returning 400 due to #{e.message}" + \
+                  " from request with env #{request.inspect}"
+
+      headers = { 'Content-Type' => "text/plain; charset=utf-8" }
+      text = "Bad Request"
+      [400, headers, [text]]
     end
   end
 
