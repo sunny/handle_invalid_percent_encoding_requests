79e6565b18cbe31bd478ff3d9af55e98cce59b04
Sunny Ripert <sunny@sunfox.org>
2019-07-27 10:57:53 +0200

    Rescue more exceptions

--- lib/handle_invalid_percent_encoding_requests/middleware.rb
+++ lib/handle_invalid_percent_encoding_requests/middleware.rb
@@ -1,5 +1,25 @@
 # Via https://gist.github.com/bf4/d26259acfa29f3b9882b#file-exception_app-rb
 module HandleInvalidPercentEncodingRequests
+  module InvalidPercentEncodingErrorMatcher
+    def self.===(error)
+      error.is_a?(ArgumentError) &&
+        error.message =~ /invalid %-encoding/
+    end
+  end
+
+  module InvalidByteSequenceErrorMatcher
+    def self.===(error)
+      error.is_a?(ArgumentError) &&
+        error.message == "invalid byte sequence in UTF-8"
+    end
+  end
+
+  module NullByteErrorMatcher
+    def self.===(error)
+      error.is_a?(ArgumentError) &&
+        error.message == "string contains null byte"
+    end
+  end
 
   # Rack Middleware inserted before the request that detects an encoding error
   # and returns an appropriate response.
@@ -17,14 +37,16 @@ module HandleInvalidPercentEncodingRequests
       # calling request.params is sufficient to trigger the error see
       # https://github.com/rack/rack/issues/337#issuecomment-46453404
       request.params
+
       @app.call(env)
 
-    # Rescue from that specific ArgumentError
-    rescue ArgumentError => e
-      raise unless e.message =~ /invalid %-encoding/
+    rescue InvalidPercentEncodingErrorMatcher,
+           InvalidByteSequenceErrorMatcher,
+           NullByteErrorMatcher
 
-      @logger.info "Bad request. Returning 400 due to #{e.message} from " + \
-                   "request with env #{request.inspect}"
+      @logger.info "Bad request. Returning 400 due to #{e.class.name} " \
+                   "#{e.message.inspect} from request with env " \
+                   "#{request.inspect}"
       error_response
     end
 
@@ -32,10 +54,9 @@ module HandleInvalidPercentEncodingRequests
     private
 
     def error_response
-      headers = { 'Content-Type' => 'text/plain; charset=utf-8' }
-      text = 'Bad Request'
+      headers = { "Content-Type" => "text/plain; charset=utf-8" }
+      text = "Bad Request"
       [400, headers, [text]]
     end
   end
-
 end
